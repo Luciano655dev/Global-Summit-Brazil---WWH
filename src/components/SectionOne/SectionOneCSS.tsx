e92bb4556cde3fa58c6b89d9bdc5e47ccc351bb2
Luciano655dev <lucianomenezes655@gmail.com>
2025-04-29 08:51:56 -0400

    c

--- src/components/SectionOne/SectionOneCSS.tsx
+++ src/components/SectionOne/SectionOneCSS.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   width: 100%;
-  min-height: 150vh;
+  min-height: 170vh;
   display: flex;
   justify-content: center;
   align-items: center;
